ccef4aac50c9085b8439ad8af14e8e1a6e4d4b1f
Grayson <morris31@colostate.edu>
2023-12-20 13:28:10 -0700

    Paralellism Algorithms

--- Algorithms/Paralellism/paraFib.java
+++ Algorithms/Paralellism/paraFib.java
@@ -1,5 +1,51 @@
 package Algorithms.Paralellism;
 
+import java.util.Scanner;
+
 public class paraFib {
-    // TODO: Implement Fibonacci using paralell programming
+
+    public int input(int n) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of terms: ");
+        n = scanner.nextInt();
+        scanner.close();
+        return n;
+    }
+
+    public int fib(int n) {
+        if (n < 2) return n;
+        else {
+            final int[] x = new int[1];
+            final int[] y = new int[1];
+
+            Thread t1 = new Thread(() -> {
+                x[0] = fib(n - 1);
+            });
+
+            Thread t2 = new Thread(() -> {
+                y[0] = fib(n - 2);
+            });
+
+            t1.start();   
+            t2.start();
+
+            try {
+                t1.join();
+                t2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return x[0] + y[0];
+        }
+    }
+
+    public static void main(String[] args) {
+        paraFib pf = new paraFib();
+        int n = 0;
+        n = pf.input(n);
+        System.out.println("Calculating Fibonacci series for " + n + " terms...");
+        for (int i = 0; i <=10; i++) {
+            System.out.println(pf.fib(i));
+        }
+    }
 }
--- Algorithms/Paralellism/paraHanoi.java
+++ Algorithms/Paralellism/paraHanoi.java
@@ -1,5 +1,45 @@
 package Algorithms.Paralellism;
 
 public class paraHanoi {
-    // TODO: Implement Towers of Hanoi using paralell programming
+    public void hanoi(int n, char from, char to, char aux) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from rod " + from + " to rod " + to);
+            return;
+        } else {
+
+            final int[] x = new int[1];
+
+            Thread t1 = new Thread(() -> {
+                hanoi(n - 1, from, aux, to);
+            });
+
+            Thread t2 = new Thread(() -> {
+                x[0] = n;
+            });
+
+            Thread t3 = new Thread(() -> {
+                hanoi(n - 1, aux, to, from);
+            });
+
+            t1.start();
+            t2.start();
+            t3.start();
+
+            try {
+                t1.join();
+                t2.join();
+                t3.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Move disk " + n + " from rod " + from + " to rod " + to);
+        }
+    }
+
+    public static void main(String[] args) {
+        paraHanoi ph = new paraHanoi();
+        int n = 4;
+        ph.hanoi(n, 'A', 'C', 'B');
+    }
 }
